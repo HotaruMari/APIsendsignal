8d332eb22f483d878d78c7a5b37b7d3bb7db9a95
HotaruMari <marisela12santi@gmail.com>
2022-11-30 14:48:36 -0600

    estructura base del proyecto

--- APIsignalup.py
+++ APIsignalup.py
@@ -34,8 +34,9 @@ async def sendMuestra(infodelsensor):
     execute(data)
     respuesta = dict()
     res = cursor.fetchall()
-    for x in range(len(res)):
-        respuesta[f'{x}']=res[x]
+    if (res):
+        for x in range(len(res)):
+            respuesta[f'{x}']=res[x]
     conn.commit( )
     respuesta['data']=infodelsensor
     return respuesta
